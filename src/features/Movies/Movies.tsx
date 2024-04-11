a44d633099964989d5485a30116b011921c74691
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-04-11 14:08:23 +0300

    custom hook

--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -2,8 +2,10 @@ import { useContext, useEffect } from 'react';
 import { Container, Grid, LinearProgress, Typography } from '@mui/material';
 
 import { useAppDispatch, useAppSelector } from '../../hooks';
+import { useIntersectionObserver } from '../../hooks/useIntersectionObserver';
 import { AuthContext, anonymousUser } from '../../AuthContext';
-import { fetchMovies } from './moviesSlice';
+// import { fetchMovies } from './moviesSlice';
+import { fetchNextPage } from './moviesSlice';
 import { MovieCard } from './MovieCard';
 
 
@@ -11,37 +13,47 @@ function Movies() {
   const dispatch = useAppDispatch();
   const movies = useAppSelector((state) => state.movies.top);
   const loading = useAppSelector((state) => state.movies.loading);
+  const hasMorePages = useAppSelector((state) => state.movies.hasMorePages);
 
   const { user } = useContext(AuthContext);
   const loggedIn = user !== anonymousUser;
 
+  const [targetRef, entry] = useIntersectionObserver();
+
+  // useEffect(() => {
+  //   dispatch(fetchMovies());
+  // }, [dispatch]);
+
   useEffect(() => {
-    dispatch(fetchMovies());
-  }, [dispatch]);
+    if (entry?.isIntersecting && hasMorePages) {
+      dispatch(fetchNextPage());
+    }
+  }, [dispatch, entry?.isIntersecting, hasMorePages]);
 
   return (
     <Container sx={{ py: 8 }} maxWidth='lg'>
       <Typography variant='h4' align='center' gutterBottom>Now playing</Typography>
 
-      {loading ? (<LinearProgress color='secondary' />) :
-        <Grid container spacing={4}>
-          {movies.map((m) => (
-            <Grid item key={m.id} xs={12} sm={6} md={4}>
-              <MovieCard
-                key={m.id}
-                id={m.id}
-                title={m.title}
-                overview={m.overview}
-                popularity={m.popularity}
-                image={m.image}
-                enableUserActions={loggedIn}
-              />
-            </Grid>
-          ))
-          }
-        </Grid>
-      }
+      <Grid container spacing={4}>
+        {movies.map((m) => (
+          <Grid item key={m.id} xs={12} sm={6} md={4}>
+            <MovieCard
+              key={m.id}
+              id={m.id}
+              title={m.title}
+              overview={m.overview}
+              popularity={m.popularity}
+              image={m.image}
+              enableUserActions={loggedIn}
+            />
+          </Grid>
+        ))
+        }
+      </Grid>
 
+      <div ref={targetRef}>
+        {loading && (<LinearProgress color='secondary' sx={{ mt: 3 }} />)}
+      </div>
     </Container>
   );
 }
