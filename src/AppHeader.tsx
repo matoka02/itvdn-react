576d1d0f9bf5ece2d22d4e272b94ea0a0f0d2538
matoka02 <a.m.nigomedzyanova@gmail.com>
2024-04-14 13:56:24 +0300

    auth0
    install

--- src/AppHeader.tsx
+++ src/AppHeader.tsx
@@ -1,9 +1,10 @@
-import { useContext } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+// import { useContext } from 'react';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { AppBar, Box, Button, Link, Toolbar, Typography } from '@mui/material';
 import LiveTvOutlinedIcon from '@mui/icons-material/LiveTvOutlined';
 
-import { AuthContext, anonymousUser } from './AuthContext';
+// import { AuthContext, anonymousUser } from './AuthContext';
+import { useAuth0 } from '@auth0/auth0-react';
 
 interface AuthHeaderProps {
   onLogin(): void;
@@ -15,7 +16,7 @@ export function AppHeader({ onLogin, onLogout }: AuthHeaderProps) {
     <AppBar position='static'>
       <Toolbar>
         <LiveTvOutlinedIcon sx={{ mr: 2 }} />
-        <Typography variant="h6" color="inherit" noWrap>The Movies DB</Typography>
+        <Typography variant='h6' color='inherit' noWrap>The Movies DB</Typography>
         <Box sx={{ flexGrow: 1 }}>
           <nav>
             <HeaderLink to='/'>Home</HeaderLink>
@@ -24,34 +25,65 @@ export function AppHeader({ onLogin, onLogout }: AuthHeaderProps) {
             <HeaderLink to='/movies'>Movies</HeaderLink>
           </nav>
         </Box>
-        <AuthSection onLogin={onLogin} onLogout={onLogout} />
+        {/* <AuthSection onLogin={onLogin} onLogout={onLogout} /> */}
+        <AuthSection/>
       </Toolbar>
     </AppBar>
   )
 }
 
-interface AuthSectionProps {
-  onLogin(): void;
-  onLogout(): void;
-}
+// interface AuthSectionProps {
+//   onLogin(): void;
+//   onLogout(): void;
+// }
 
-function AuthSection({ onLogin, onLogout }: AuthSectionProps) {
+// function AuthSection({ onLogin, onLogout }: AuthSectionProps) {
 
-  const auth = useContext(AuthContext);
-  const loggedIn = auth.user !== anonymousUser;
+//   const auth = useContext(AuthContext);
+//   const loggedIn = auth.user !== anonymousUser;
 
-  if (loggedIn) {
+//   if (loggedIn) {
+//     return (<>
+//       <Typography>Hello, {auth.user.name}</Typography>
+//       <Button color='inherit' variant='outlined' onClick={onLogout}>Log out</Button>
+//     </>)
+//   }
+//   return <Button color='inherit' variant='outlined' onClick={onLogin} sx={{ ml: 1.5 }}>Log in</Button>
+// }
+
+function AuthSection() {
+  const { loginWithRedirect, isAuthenticated, user, logout } = useAuth0();
+  // const navigate = useNavigate();
+
+  const onLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: '/',
+      },
+    });
+  };
+
+  const onLogout = () => {
+    logout({
+      logoutParams: {
+        returnTo: window.location.origin,
+      },
+    });
+  };
+
+  if (isAuthenticated) {
     return (<>
-      <Typography>Hello, {auth.user.name}</Typography>
+      <Typography>Hello, {user?.name}</Typography>
       <Button color='inherit' variant='outlined' onClick={onLogout}>Log out</Button>
     </>)
   }
   return <Button color='inherit' variant='outlined' onClick={onLogin} sx={{ ml: 1.5 }}>Log in</Button>
+
 }
 
 function HeaderLink({ to, children }: { to: string, children: React.ReactNode }) {
   return (
-    <Link component={RouterLink} to={to} variant="button" color="inherit" sx={{ my: 1, mx: 1.5 }}>
+    <Link component={RouterLink} to={to} variant='button' color='inherit' sx={{ my: 1, mx: 1.5 }}>
       {children}
     </Link>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -17,17 +17,21 @@ import Home from './features/Home/Home';
 import About from './features/About/About';
 import { Extra } from './features/Extra/Extra';
 import { ErrorBoundary } from './ErrorBoundary';
+import { StatefulAuthProvider } from './auth/StatefulAuthProvider';
+import { AuthCallback } from './auth/AuthCallback';
 
 
 const Movies = lazy(() => import('./features/Movies/Movies'));
 
 function AppEntrypoint() {
   return (
-    <Provider store={store}>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </Provider>
+    <StatefulAuthProvider>
+      <Provider store={store}>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </Provider>
+    </StatefulAuthProvider>
   );
 }
 
@@ -45,7 +49,8 @@ const router = createBrowserRouter([
             <Movies />
           </Suspense>
         )
-      }
+      },
+      { path: 'callback', element: <AuthCallback/> },
     ],
   },
 ]);
